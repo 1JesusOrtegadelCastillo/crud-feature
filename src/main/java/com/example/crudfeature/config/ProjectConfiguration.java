caea60803bc673fd85cc978586f4a4d499a0004f
Jesus Ortega del Castillo <52591802+1JesusOrtegadelCastillo@users.noreply.github.com>
2022-05-03 13:42:09 -0700

    Update ProjectConfiguration.java

--- src/main/java/com/example/crudfeature/config/ProjectConfiguration.java
+++ src/main/java/com/example/crudfeature/config/ProjectConfiguration.java
@@ -5,6 +5,5 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "service")
 public class ProjectConfiguration {
 }
